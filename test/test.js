d38e06800e5c6ddb5e6f979dfdc21d2b2ff9a325
BugHunterX2025 <devsaurabh@sheelinfotech.in>
2025-01-19 12:17:59 +0530

    saurfabh

---
+++ test/test.js
@@ -0,0 +1,21 @@
+const autocannon = require("autocannon");
+const urls = ["http://localhost:3000", "http://localhost:3000/stress-test"];
+const duration = 30; // specify the duration for the test in seconds
+urls.forEach((url) => {
+  const inst = autocannon(
+    {
+      url,
+      duration: duration,
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`url : ${url}`);
+        console.log("no or request", result.requests.total);
+        console.log("dureation", result.duration);
+      }
+    }
+  );
+  autocannon.track(inst);
+});
